fff9d9d7f3c767f5a3df09d36b732d9436c44bff
KDH5706 <rpaehdgns12@naver.com>
2022-06-30 17:11:58 +0900

    220630 commit

---
+++ Socket/sample.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+void error_handling(char *message);
+
+int main(int argc, char *argv[])
+{
+	return 0;
+}
+
+void error_handling(char *message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
